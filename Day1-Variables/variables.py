fc5479c69c71447972fbf52c408fd8d8756235aa
ngobahoangquan <hoangquan121202@gmail.com>
2023-10-18 01:08:26 +0700

    commit

---
+++ Day1-Variables/variables.py
@@ -0,0 +1,19 @@
+#There is two variables a and b from input
+a = input()
+b = input()
+
+
+## Do not change the code above
+####################################
+#Write your code below this line to get output a: Hello and  b: World
+
+a = "Hello"
+b = "World"
+
+
+
+#Write your code above this line
+print("a: " + a)
+print("b: " + b)
+
+#NOTICE: Bạn có thể đặt tên biến tùy ý thích tuy nhiên cũng cần tuân thủ một số quy tắc để không gây ra lỗi chương trình
