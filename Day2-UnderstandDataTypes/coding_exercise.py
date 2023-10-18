f3367044ca8e1529751a18da601dba990a25197f
ngobahoangquan <hoangquan121202@gmail.com>
2023-10-18 13:32:22 +0700

    commit

--- Day2-UnderstandDataTypes/coding_exercise.py
+++ Day2-UnderstandDataTypes/coding_exercise.py
@@ -12,12 +12,9 @@ two_digits_number = input()
 #Kiểm tra kiểu dữ liệu nhập vào
 print(type(two_digits_number))
 
-#Bởi vì là kiểu integer nên không thể lấy từng index trong giá trị mà phải ép kiểu về string
-str_two_digits_number = str(two_digits_number)
-
-#Lấy từng phần tử trong string mới và ép kiểu lại về integer
-two_digits_number_1 = int(str_two_digits_number[0])
-two_digits_number_2 = int(str_two_digits_number[1])
+#Lấy từng phần tử trong string vừa input và ép kiểu lại về integer
+two_digits_number_1 = int(two_digits_number[0])
+two_digits_number_2 = int(two_digits_number[1])
 
 #Sau đó in ra tổng của 2 biến vừa có được là kết quả của bài toán
 print(two_digits_number_1 + two_digits_number_2)
