2616ede873ca4eae1a2a3b0857bdde32521cf110
ngobahoangquan <hoangquan121202@gmail.com>
2023-10-18 10:56:04 +0700

    gitcommit

---
+++ Day1-Variables/project.py
@@ -0,0 +1,7 @@
+#Hãy tự viết chương trình nhập xuất đầu tiên của bạn
+#Đề bài: Project Band Name Generator 
+
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
---
+++ Day2-UnderstandDataTypes/datatypes.py
@@ -0,0 +1,22 @@
+#DataTypes
+
+#Sring
+#Kiểu dữ liệu string sẽ luôn nằm trong ""
+print("Hello"[4]) #Ta có thể lấy được index của từng ký tự trong kiểu string
+print(len("Hello"))
+print("123" + "456") #output: 123456 
+#Bởi nằm trong ngoặc kép "" nên 123 và 456 không phải là số mà là chuỗi
+
+#Integer
+print(123 + 456)
+#Khi này màn hình sẽ in ra 579 là đáp án của phép tính 123 + 456 bởi khi này 123 và 456 là kiểu dữ liệu số
+
+#Float
+#Float là kiểu dữ liệu số thập phân có dấu phẩy động
+print(3.14)
+
+#Boolean
+#Kiểu dữ liệu Boolean chỉ có 2 giá trị True và False
+isTest = False
+print(isTest)
+#Khi này isTest sẽ là kiểu dữ liệu boolean
