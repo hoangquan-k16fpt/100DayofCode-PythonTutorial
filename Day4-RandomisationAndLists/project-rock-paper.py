8f484657016f71b44f483247233a9f1b9a37d084
ngobahoangquan <hoangquan121202@gmail.com>
2023-10-23 14:41:28 +0700

    gitcomit

--- Day4-RandomisationAndLists/project-rock-paper.py
+++ Day4-RandomisationAndLists/project-rock-paper.py
@@ -35,23 +35,22 @@ game_img = [rock, papers, scissors]
 #Sử dụng câu lệnh điều kiện để tạo ra game, luật chơi tương tự như trò chơi kéo bao búa
 #Để có thể in được game_img, ta sử dụng lệnh print và gọi phần tử trong list tương ứng với phần tử mà user hoặc computer đã chọn
 user_choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors.\n"))
-print(game_img[user_choose])
-computer_choose = random.randint(0, 2)
-print (f"Computer choose: {computer_choose}")
-print(game_img[computer_choose])
-if user_choose == 0 and computer_choose == 2:
-    print("You win!")
-elif computer_choose == 0 and user_choose == 2:
-    print("You lose!")
-elif computer_choose > user_choose:
-    print("You lose!")
-elif user_choose >= 3:
+
+if user_choose >= 3 or user_choose < 0:
     print("You typed an invalid number, you lose!")
-elif computer_choose < user_choose:
-    print("You win!")
-elif computer_choose == user_choose:
-    print("It's draw!")
-
-
-
-    
+else:
+    print(f"Your choice: {user_choose}")
+    print(game_img[user_choose])
+    computer_choose = random.randint(0, 2)
+    print (f"Computer choose: {computer_choose}")
+    print(game_img[computer_choose])
+    if user_choose == 0 and computer_choose == 2:
+        print("You win!")
+    elif computer_choose == 0 and user_choose == 2:
+        print("You lose!")
+    elif computer_choose > user_choose:
+        print("You lose!")
+    elif computer_choose < user_choose:
+        print("You win!")
+    elif computer_choose == user_choose:
+        print("It's draw!")
